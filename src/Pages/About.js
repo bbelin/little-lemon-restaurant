350ed45ffb46fda9afd6815dcf4e14abce680989
Blagovest Belin <btbelin@gmail.com>
2025-05-03 00:56:17 +0300

    add headings to empty pages

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,6 +6,15 @@ export const About = () => {
   return (
     <>
       <Main>
+        <section className='component-section'>
+          <div className='grid-container'>
+            <div className='col-4 tab-col-8 desk-col-2'></div>
+            <div className='col-4 tab-col-8 desk-col-4'>
+              <h2>About page</h2>
+            </div>
+            <div className='col-4 tab-col-8 desk-col-2'></div>
+          </div>
+        </section>
       </Main>
       <Footer />
     </>
